c77e6cf718f681ed4114e377bf45c406a89b8cec
Angga7Togk <kiplihode123321@gmail.com>
2025-01-05 16:29:05 +0700

    Change PluginOwnedTrait

--- src/angga7togk/mineshop/command/MineCommand.php
+++ src/angga7togk/mineshop/command/MineCommand.php
@@ -8,53 +8,59 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\permission\DefaultPermissions;
 use pocketmine\player\Player;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\PluginOwnedTrait;
 use pocketmine\utils\TextFormat;
 
-class MineCommand extends Command
+class MineCommand extends Command implements PluginOwned
 {
+    use PluginOwnedTrait;
 
-  public function __construct(private MineShop $plugin)
-  {
-    parent::__construct('mineshop', 'Open the mine shop menu', '/mineshop', ['mshop']);
-    $this->setPermission('mineshop.command');
-  }
+    public function __construct(MineShop $plugin)
+    {
+        parent::__construct('mineshop', 'Open the mine shop menu', '/mineshop', ['mshop']);
+        $this->setPermission('mineshop.command');
 
-  public function execute(CommandSender $sender, string $commandLabel, array $args)
-  {
-    if (!$sender instanceof Player) {
-      $sender->sendMessage(MineShop::$PREFIX . TextFormat::RED . 'Please use this command in-game!');
-      return;
+        $this->owningPlugin = $plugin;
     }
-    if (isset($args[0])) {
-      switch (strtolower($args[0])) {
-        case 'sell':
-        case 'set':
-        case 'add':
-          if ($sender->getInventory()->getItemInHand()->isNull()) {
-            $sender->sendMessage(MineShop::$PREFIX . TextFormat::RED . 'Please hold item in your hand!');
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void
+    {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage(MineShop::$PREFIX . TextFormat::RED . 'Please use this command in-game!');
             return;
-          }
-          if ($sender->hasPermission('mineshop.command.sell') || $sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
-            (new MineMenu($this->plugin))->sellMenu($sender);
-            return;
-          }
-          break;
-        case 'unsell':
-        case 'remove':
-        case 'delete':
-          if ($sender->hasPermission('mineshop.command.unsell') || $sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
-            (new MineMenu($this->plugin))->open($sender, true);
-            return;
-          }
-          break;
-        default:
-          if ($sender->hasPermission('mineshop.command.sell') || $sender->hasPermission('mineshop.command.unsell') || $sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
-            $sender->sendMessage(MineShop::$PREFIX . TextFormat::RED . 'Usage: /mineshop <sell|unsell>');
-          }
-          break;
-      }
-    } else {
-      (new MineMenu($this->plugin))->open($sender);
+        }
+
+        if (isset($args[0])) {
+            switch (strtolower($args[0])) {
+                case 'sell':
+                case 'set':
+                case 'add':
+                    if ($sender->getInventory()->getItemInHand()->isNull()) {
+                        $sender->sendMessage(MineShop::$PREFIX . TextFormat::RED . 'Please hold item in your hand!');
+                        return;
+                    }
+                    if ($sender->hasPermission('mineshop.command.add') || $sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
+                        (new MineMenu($this->owningPlugin))->sellMenu($sender);
+                        return;
+                    }
+                    break;
+                case 'unsell':
+                case 'remove':
+                case 'delete':
+                    if ($sender->hasPermission('mineshop.command.delete') || $sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
+                        (new MineMenu($this->owningPlugin))->open($sender, true);
+                        return;
+                    }
+                    break;
+                default:
+                    if ($sender->hasPermission('mineshop.command.add') || $sender->hasPermission('mineshop.command.delete') || $sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
+                        $sender->sendMessage(MineShop::$PREFIX . TextFormat::RED . 'Usage: /mineshop <set|edit|delete>');
+                    }
+                    break;
+            }
+        } else {
+            (new MineMenu($this->owningPlugin))->open($sender);
+        }
     }
-  }
 }
